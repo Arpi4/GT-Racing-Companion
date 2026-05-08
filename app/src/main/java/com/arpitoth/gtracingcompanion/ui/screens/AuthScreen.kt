0f31555d0ae538b3fe416755a5575d71f71faa92
Tóth Árpád Zsolt <toth.arpad223@gmail.com>
2026-05-08 09:02:10 +0200

    Update

--- app/src/main/java/com/arpitoth/gtracingcompanion/ui/screens/AuthScreen.kt
+++ app/src/main/java/com/arpitoth/gtracingcompanion/ui/screens/AuthScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.navigation.NavController
 import com.arpitoth.gtracingcompanion.data.AuthRepository
 import com.arpitoth.gtracingcompanion.ui.theme.Spacing
@@ -56,6 +57,7 @@ fun AuthScreen(
             value = password,
             onValueChange = { password = it },
             label = { Text("Jelszó") },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier
                 .fillMaxWidth()
                 .testTag("auth_password")
